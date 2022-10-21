fe6ef09a6907255e758ce93d3e78f29ccdb15696
m41w4r3.exe <33025255+loopbender@users.noreply.github.com>
2022-10-21 18:47:30 +0200

    remove year dropping

--- best_pipeline.py
+++ best_pipeline.py
@@ -28,9 +28,6 @@ def best_pipeline_intown(trainX):  ### returns predicted Y
     numerical_features = [value for value in all_numerical_features]
     categorical_features = [value for value in all_categorical_features]
 
-    numerical_features.remove("year")
-    # categorical_features.append("year")
-
     # Preprocessing for numerical data
     numerical_transformer = Pipeline(
         steps=[
