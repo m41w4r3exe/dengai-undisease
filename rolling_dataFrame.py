b141bba8a1e8d840502d9ce3055dd6a991f3ae43
Jean Simonnet <jeanasimonnet@gmail.com>
2022-10-21 17:30:12 +0200

    rolling_dataFrame.py

---
+++ rolling_dataFrame.py
@@ -0,0 +1,5 @@
+def rolling_dataframe_baby(df, n=2):
+    df_rolled = df.rolling(n).mean()
+    df_rolled.loc[:, "year"] = df.loc[:, "year"]
+    df_rolled.loc[:, "weekofyear"] = df.loc[:, "weekofyear"]
+    return df_rolled
