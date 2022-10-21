3e872ac3c6fe1e475b56161dd43b3e42e8106b1f
Jean Simonnet <jeanasimonnet@gmail.com>
2022-10-21 18:12:31 +0200

    a few new dev

--- rolling_dataFrame.py
+++ rolling_dataFrame.py
@@ -3,3 +3,10 @@ def rolling_dataframe_baby(df, n=2):
     df_rolled.loc[:, "year"] = df.loc[:, "year"]
     df_rolled.loc[:, "weekofyear"] = df.loc[:, "weekofyear"]
     return df_rolled
+
+
+def lagging_columns(df):
+    df_rolled = df.rolling(n).mean()
+    df_rolled.loc[:, "year"] = df.loc[:, "year"]
+    df_rolled.loc[:, "weekofyear"] = df.loc[:, "weekofyear"]
+    return df_rolled
