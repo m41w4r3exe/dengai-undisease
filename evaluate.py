2ebd2ffa48b78701b93ddbd06cd6a1243df12867
louis-demetz <61187384+louis-demetz@users.noreply.github.com>
2022-10-21 16:33:44 +0200

    tracking function to save pipeline score and parms

--- evaluate.py
+++ evaluate.py
@@ -17,12 +17,29 @@ def evaluate(pipeline, X, y):
     mae = -cv_results["test_neg_mean_absolute_error"]
     print(f"Mean Absolute Error:     {mae.mean():.3f} +/- {mae.std():.3f}\n")
 
+    return f' MAE : {mae.mean():.3f} +/- {mae.std():.3f}'
+
 
 def evaluate_for_city(city):
     print(f"\nResults for {city.upper()}:")
     X, y = getTrainData(city)
-    evaluate(best_pipeline_intown(X), X, y)
+    score = evaluate(best_pipeline_intown(X), X, y)
+
+    return f'{city} {score}'
+
+def get_save_params(score_sj, score_iq):
+    X, y = getTrainData('sj')
+    pipeline = best_pipeline_intown(X)
+    data = str(pipeline.named_steps)
+    data = data.replace('\n', '').replace('  ', '')
+    data = score_sj + ', ' + score_iq + ', ' + data
+
+    with open("results.txt", "a") as myfile:
+        myfile.write(f'\n{data}')
 
 
-evaluate_for_city("sj")
-evaluate_for_city("iq")
+if __name__ == "__main__":
+
+    score_sj = evaluate_for_city("sj")
+    score_iq = evaluate_for_city("iq")
+    get_save_params(score_sj, score_iq)
