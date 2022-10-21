e6538d84508281dd6da281b59f9f6aac3987a93e
louis-demetz <61187384+louis-demetz@users.noreply.github.com>
2022-10-21 17:16:20 +0200

    calculating and storing weighted average

--- evaluate.py
+++ evaluate.py
@@ -17,29 +17,39 @@ def evaluate(pipeline, X, y):
     mae = -cv_results["test_neg_mean_absolute_error"]
     print(f"Mean Absolute Error:     {mae.mean():.3f} +/- {mae.std():.3f}\n")
 
-    return f' MAE : {mae.mean():.3f} +/- {mae.std():.3f}'
+    return mae.mean(), mae.std()
 
 
 def evaluate_for_city(city):
     print(f"\nResults for {city.upper()}:")
     X, y = getTrainData(city)
-    score = evaluate(best_pipeline_intown(X), X, y)
+    mae_mean, mae_std = evaluate(best_pipeline_intown(X), X, y)
+    text_score= f'{city} - MAE : {mae_mean:.3f} +/- {mae_std:.3f}'
 
-    return f'{city} {score}'
+    return text_score, mae_mean, mae_std
 
-def get_save_params(score_sj, score_iq):
-    X, y = getTrainData('sj')
-    pipeline = best_pipeline_intown(X)
+def get_save_params(score_sj, score_iq, mae_mean_sj, mae_std_sj, mae_mean_iq, mae_std_iq):
+
+    # Calculate scores
+    Xsj, y = getTrainData('sj')
+    Xiq, y = getTrainData('iq')
+    isj, iiq, itot = len(Xsj), len(Xiq), len(Xsj) + len(Xiq)
+    weighted_mean = mae_mean_sj * isj / itot + mae_mean_iq * iiq / itot
+    weighted_std = mae_mean_sj * isj / itot + mae_mean_iq * iiq / itot
+
+    # Get pipeline params and clean them
+    pipeline = best_pipeline_intown(Xsj)
     data = str(pipeline.named_steps)
     data = data.replace('\n', '').replace('  ', '')
-    data = score_sj + ', ' + score_iq + ', ' + data
+    data = f'weighted average MAE: {weighted_mean:.3f} +/- {weighted_std:.3f}, '  + str(score_sj) + ', ' + str(score_iq) + ', ' + data
 
+    # save params and score in file
     with open("results.txt", "a") as myfile:
         myfile.write(f'\n{data}')
 
 
 if __name__ == "__main__":
 
-    score_sj = evaluate_for_city("sj")
-    score_iq = evaluate_for_city("iq")
-    get_save_params(score_sj, score_iq)
+    score_sj, mae_mean_sj, mae_std_sj = evaluate_for_city("sj")
+    score_iq, mae_mean_iq, mae_std_iq = evaluate_for_city("iq")
+    get_save_params(score_sj, score_iq, mae_mean_sj, mae_std_sj, mae_mean_iq, mae_std_iq)
