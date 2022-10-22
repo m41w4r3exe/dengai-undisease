34c741859315fb5d2490bea5aaaf16f081e4edd9
Jean Simonnet <jeanasimonnet@gmail.com>
2022-10-22 14:56:17 +0200

    upodate readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dengai-undisease
 Predicting against the damn mosquitos
+https://www.drivendata.org/competitions/44/dengai-predicting-disease-spread/page/82/
+
+![Epidemics over time](./pictures_readme/Epidemic_Evolution_0ver_time.png)
+
--- smote_oversample.py
+++ smote_oversample.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from imblearn.over_sampling import SMOTE
+from imblearn.pipeline import make_pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+
 
 def add_norm_cases_and_peak_to_df(
     X, 
@@ -31,12 +35,29 @@ def add_norm_cases_and_peak_to_df(
     return X
 
 
+    def oversample_smote_X(join_X_Y, on='peak'):
+        resampling_col = join_X_Y.loc[:, on]
+        dropped_join_X_Y = join_X_Y.drop(columns=["peak", "city", "week_start_date"])
+        # dropped_join_X_Y = join_X_Y.drop(columns=["peak")
+
+        pipe = make_pipeline(
+            SimpleImputer(),
+            SMOTE(sampling_strategy=0.5, random_state=42),
+        )
+        upsampled_X, upsampled_y = pipe.fit_resample(X, resampling_col)
+        return upsampled_X
+
+
+
+ 
+
 ##
 official_trainX = pd.read_csv("./data/dengue_features_train.csv")
 official_trainY = pd.read_csv("./data/dengue_labels_train.csv")
 official_trainX = add_norm_cases_and_peak_to_df(official_trainX, official_trainY)
-official_trainX.head()
 
 
+official_trainX['peak'].value_counts()/
 
 
+oversample_smote_X_Y(official_trainX)
