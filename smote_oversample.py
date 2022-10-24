255ada1c29ef2ae7a54700308d9e97259cab0cb4
Jean Simonnet <jeanasimonnet@gmail.com>
2022-10-25 00:23:15 +0200

    smote_oversample

--- smote_oversample.py
+++ smote_oversample.py
@@ -1,10 +1,10 @@
 # imports
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-from imblearn.over_sampling import SMOTE, SMOTENC
+from imblearn.over_sampling import SMOTE
 from sklearn.preprocessing import OrdinalEncoder
 from imblearn.pipeline import make_pipeline
-from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 
 
@@ -46,7 +46,7 @@ def plot_peaks_on_time_series(
         "o",
         color="red",
     )
-    plt.title(city, color=color)
+    plt.title(f"city: {city}", color=color)
     plt.xlabel("date")
     plt.ylabel("normalized case numbers")
     plt.axis("tight")
@@ -54,7 +54,7 @@ def plot_peaks_on_time_series(
 
 
 def histo_norm_cases(combined_X_Y):
-    plt.hist(combined_X_Y["total_cases_normalized"], 20, color="black")
+    plt.hist(combined_X_Y["total_cases_normalized"], 40, color="grey")
     plt.title("distribution of case numbers in dataset")
     plt.xlabel("normalized case numbers")
     plt.ylabel("count")
@@ -63,44 +63,44 @@ def histo_norm_cases(combined_X_Y):
     pass
 
 
-def add_norm_cases_and_peak_to_df(combined_X_Y: pd.DataFrame, plot_peaks: bool = True):
+def add_norm_cases_and_peak_to_df(
+    combined_X_Y: pd.DataFrame,
+    city: str = "cityname",
+    color: str = "purple",
+    plot_peaks: bool = True,
+):
     # Adding a column "normalized_case_numbers" and
     # Adding a column "peak" labelling the outbreaks of the epidemics with 1
     peak_threshold = 0.08
-    colors = ["purple", "orange"]
-    combined_X_Y["total_cases_normalized"] = combined_X_Y["total_cases"]
+    combined_X_Y.insert(
+        len(combined_X_Y.columns),
+        "total_cases_normalized",
+        combined_X_Y.loc[:, "total_cases"],
+    )
 
-    # need to be done city by city
-    cities = combined_X_Y["city"].unique()
-    for c, color in zip(cities, colors):
-        ## converting numbers / ratio with inhabitants
-        max_cases_this_city = (
-            combined_X_Y["total_cases"].loc[combined_X_Y["city"] == c].max()
-        )
-        indices = combined_X_Y["total_cases"].loc[combined_X_Y["city"] == c].index
-        for i in indices:
-            combined_X_Y.loc[i, "total_cases_normalized"] = (
-                combined_X_Y.loc[i, "total_cases"]
-            ) / max_cases_this_city
-
-        if plot_peaks:
-            plot_peaks_on_time_series(
-                combined_X_Y, city=c, color=color, peak_threshold=peak_threshold
-            )
+    ## converting numbers / ratio with inhabitants
+    max_cases = combined_X_Y["total_cases"].max()
+    indices = combined_X_Y["total_cases"].index
+    for i in indices:
+        combined_X_Y.loc[i, "total_cases_normalized"] = (
+            combined_X_Y.loc[i, "total_cases"]
+        ) / max_cases
 
     if plot_peaks:
+        plot_peaks_on_time_series(
+            combined_X_Y, city=city, color=color, peak_threshold=peak_threshold
+        )
         histo_norm_cases(combined_X_Y)
 
-    # adding "peak" column to dataframe -> defined to categorize the target as epidemic peak (1) or not ()0
-    combined_X_Y["peak"] = combined_X_Y["total_cases_normalized"]
+    # adding "peak" column to dataframe -> defined to categorize the target as epidemic peak (1) or not (0)
+    combined_X_Y.insert(combined_X_Y.shape[1], "peak", np.zeros(combined_X_Y.shape[0]))
+    # combined_X_Y["peak"] = combined_X_Y["total_cases_normalized"]
     combined_X_Y.loc[
         (combined_X_Y["total_cases_normalized"] >= peak_threshold, "peak")
     ] = 1
     combined_X_Y.loc[
         (combined_X_Y["total_cases_normalized"] < peak_threshold, "peak")
     ] = 0
-
-    # print(combined_X_Y["peak"].value_counts())
     return combined_X_Y
 
 
@@ -108,8 +108,9 @@ def encode_cat_features(combined_X_Y):
     all_categorical_features = combined_X_Y.select_dtypes(include=[object]).columns
     categorical_features = [value for value in all_categorical_features]
     cat_encoder = OrdinalEncoder()
+    combined_X_Y_pre = combined_X_Y.copy()
     combined_X_Y.loc[:, categorical_features] = cat_encoder.fit_transform(
-        combined_X_Y.loc[:, categorical_features]
+        combined_X_Y[categorical_features]
     )
     return combined_X_Y, cat_encoder, categorical_features
 
@@ -131,6 +132,9 @@ def smote_oversample_of_peaks(combined_X_Y, based_on="peak"):
     )
     upsampled_X_Y, upsampled_col = pipe.fit_resample(combined_X_Y, resampling_col)
     upsampled_X_Y = pd.DataFrame(upsampled_X_Y, columns=combined_X_Y.columns)
+
+    # reorder and reindex by date
+    upsampled_X_Y = upsampled_X_Y.sort_values(by=["year", "weekofyear"])
     return upsampled_X_Y
 
 
@@ -144,13 +148,41 @@ def split_X_Y(upsampled_X_Y):
 def smote_oversample_run_all():
     X, Y = data_and_target_df_import()
     combined_X_Y = combine_X_and_Y_dataFrames(X, Y)
-    combined_X_Y = add_norm_cases_and_peak_to_df(combined_X_Y)
-    combined_X_Y, cat_encoder, categorical_features = encode_cat_features(combined_X_Y)
-    upsampled_X_Y = smote_oversample_of_peaks(combined_X_Y, based_on="peak")
-    upsampled_X_Y = decode_cat_features(
-        upsampled_X_Y, cat_encoder, categorical_features
-    )
-    X_upsampled, Y_upsampled = split_X_Y(upsampled_X_Y)
+
+    colors = ["purple", "orange"]
+    cities = set(combined_X_Y["city"].unique())
+    X_upsampled = pd.DataFrame()
+    Y_upsampled = pd.DataFrame()
+    for c, color in zip(cities, colors):
+        this_city_combined_X_Y = pd.DataFrame()
+        this_city_upsampled_X_Y = pd.DataFrame()
+        this_city_X_upsampled = pd.DataFrame()
+        this_city_Y_upsampled = pd.DataFrame()
+
+        this_city_combined_X_Y = combined_X_Y.loc[combined_X_Y["city"] == c, :]
+        this_city_combined_X_Y = add_norm_cases_and_peak_to_df(
+            this_city_combined_X_Y, city=c, color=color
+        )
+        this_city_combined_X_Y, cat_encoder, categorical_features = encode_cat_features(
+            this_city_combined_X_Y
+        )
+        this_city_upsampled_X_Y = smote_oversample_of_peaks(
+            this_city_combined_X_Y, based_on="peak"
+        )
+        this_city_upsampled_X_Y = decode_cat_features(
+            this_city_upsampled_X_Y, cat_encoder, categorical_features
+        )
+        this_city_X_upsampled, this_city_Y_upsampled = split_X_Y(
+            this_city_upsampled_X_Y
+        )
+
+        X_upsampled = pd.concat(
+            [X_upsampled, this_city_X_upsampled], axis=0, ignore_index=True
+        )
+        Y_upsampled = pd.concat(
+            [Y_upsampled, this_city_Y_upsampled], axis=0, ignore_index=True
+        )
+
     return X_upsampled, Y_upsampled
 
 
@@ -160,6 +192,6 @@ def smote_oversample_run_all():
 #     print(Y_upsampled.head())
 
 
-X_upsampled, Y_upsampled = smote_oversample_run_all()
-print(X_upsampled.head())
-print(Y_upsampled.head())
+# X_upsampled, Y_upsampled = smote_oversample_run_all()
+# print(X_upsampled.head())
+# print(Y_upsampled.head())
